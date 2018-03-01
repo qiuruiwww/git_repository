427df0284d476498449d3fc17451b6f9e1a033c2
qr <qiurui870554>
2018-03-01 14:04:52 +0800

    dev修改列子

--- gitTest/src/com/company/Main.java
+++ gitTest/src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a=10;
-        String abc="ccccccccccccc";
+        String abc="ccaaabbbcccccccc";
         System.out.println("Git测试程序!");
     }
 }
